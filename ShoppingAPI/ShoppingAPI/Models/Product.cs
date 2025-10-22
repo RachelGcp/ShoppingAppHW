593399e9fbac2068a625a77ce530e15ad19b8eb1
RachelGcp <rysy3692@gmail.com>
2025-10-22 17:52:42 +0300

    add dateofbirth property

--- ShoppingAPI/ShoppingAPI/Models/Product.cs
+++ ShoppingAPI/ShoppingAPI/Models/Product.cs
@@ -7,5 +7,6 @@ namespace ShoppingApp.Models
         public string Name { get; set; } = string.Empty;
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; } = null!;
+        public Datetime DateOfBirth { get; set; }
     }
 }
