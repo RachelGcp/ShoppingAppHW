23810df8185c04da47d712e5bf4f29d4fe86bc88
RachelGCP <rysy3692@gmail.com>
2025-11-01 23:23:20 +0200

    add created at

--- ShoppingAPI/ShoppingAPI/Models/Category.cs
+++ ShoppingAPI/ShoppingAPI/Models/Category.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public dateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
