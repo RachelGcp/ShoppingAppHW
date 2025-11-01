a3437f87b089ef6156818bcf6b796258d91c3812
RachelGcp <rysy3692@gmail.com>
2025-11-02 00:07:58 +0200

    add props (#3)
    * try push
    
    * add new prprties

--- ShoppingAPI/ShoppingAPI/Models/Category.cs
+++ ShoppingAPI/ShoppingAPI/Models/Category.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty; 
+        public decimal DiscountPercentage { get; set; }
         public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
